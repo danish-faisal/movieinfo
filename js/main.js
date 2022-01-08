ac4cb7f820edc0b8297dd2d6c32d4826456d5747
= <danishfaisal.wwe@gmail.com>
2022-01-08 22:20:39 +0530

    Adding Overlay

--- js/main.js
+++ js/main.js
@@ -327,4 +327,15 @@ function pageCall(pageNo) {
 
 function checkVideos(id) {
     console.log(id);
+    openNav();
+}
+
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
 }
